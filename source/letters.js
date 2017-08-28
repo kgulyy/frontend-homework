c55d1f0c77c1ba15cd3fc593197323e0ee6b0ca6
Konstantin Gulyy <kgulyy@my.com>
2017-08-28 13:42:10 +0300

    Refactoring

--- source/letters.js
+++ source/letters.js
@@ -3,15 +3,15 @@
 function letters(str, flag) {
     if (flag === undefined) {
         // удаляет все повторяющиеся символы
-        return str.split('').filter(function (x, n, s) {
-            return s.indexOf(x) === s.lastIndexOf(x)
+        return str.split('').filter(function (symbol, curPos, array) {
+            return array.indexOf(symbol) === array.lastIndexOf(symbol)
         }).join('');
     }
 
     if (flag) {
         // удаляет повторяющиеся символы, оставляет первый
-        return str.split('').filter(function (x, n, s) {
-            return s.indexOf(x) === n
+        return str.split('').filter(function (symbol, curPos, array) {
+            return array.indexOf(symbol) === curPos
         }).join('');
     } else {
         // удаляет повторяющиеся символы, оставляет последний
